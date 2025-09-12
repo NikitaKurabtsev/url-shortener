92322b66a158c863e66503b82f515b9340b18972
nikitakurabtsev <nick3500hunna@gmail.com>
2025-09-12 13:26:15 +0300

    add setupLogger and initialize logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"github.com/NikitaKurabtsev/url-shortener/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
 	// TODO: init config: cleanenv
+	cfg := config.InitConfig()
+	fmt.Println(cfg)
 
 	// TODO: init logger: slog
+	logger := setupLogger(envDev)
 
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, chi render
 
 	// TODO: init run server: net/http
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
