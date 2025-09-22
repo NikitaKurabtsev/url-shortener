c2d4e8ea9de6922de2c2f7b85a3bf3879a8883c8
nikitakurabtsev <nick3500hunna@gmail.com>
2025-09-22 10:07:12 +0300

    add redirect handler

---
+++ internal/http-server/handlers/urls/redirect/redirect.go
@@ -0,0 +1,62 @@
+package redirect
+
+import (
+	"errors"
+	"github.com/NikitaKurabtsev/url-shortener/internal/lib/api/response"
+	"github.com/NikitaKurabtsev/url-shortener/internal/lib/logger/sl"
+	"github.com/NikitaKurabtsev/url-shortener/internal/storage"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"log/slog"
+	"net/http"
+)
+
+//go:generate go run github.com/vektra/mockery/v2@v2.53.5 --name=URLGetter
+type URLGetter interface {
+	GetURL(alias string) (string, error)
+}
+
+func New(log *slog.Logger, urlSaver URLGetter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		op := "handlers.urls.redirect.New"
+
+		log = slog.With(
+			slog.With("op", op),
+			slog.With("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error("alias is empty"))
+
+			return
+		}
+
+		url, err := urlSaver.GetURL(alias)
+		if errors.Is(err, storage.ErrUrlNotFound) {
+			log.Info("url not found", "alias", alias)
+
+			render.Status(r, http.StatusNotFound)
+			render.JSON(w, r, response.Error("not found"))
+
+			return
+		}
+
+		if err != nil {
+			log.Error("failed to fetch url", sl.Err(err))
+
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, response.Error("internal error"))
+
+			return
+		}
+
+		log.Info("found url", slog.String("url", url))
+
+		http.Redirect(w, r, url, http.StatusFound)
+	}
+}
